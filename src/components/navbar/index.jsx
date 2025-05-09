877a97e45751bc86a06100c9e1034e539620bc04
SantoshP2002 <santoshpawar96k@gmail.com>
2025-05-09 18:04:26 +0530

    navbar completed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { navMapData } from "../../constants";
 import Logo from "./components/Logo";
 import SearchBar from "./components/SearchBar";
@@ -5,7 +6,8 @@ import { BsHandbag } from "react-icons/bs";
 import { CiUser } from "react-icons/ci";
 
 const Navbar = () => {
-  
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
     <div className="flex flex-col gap-4 w-full h-full py-2 px-20">
       <div className="flex items-center justify-between w-full">
@@ -22,12 +24,31 @@ const Navbar = () => {
         {navMapData.map((item, index) => (
           <div
             key={index}
-            className="group relative flex items-center gap-5 text-sm cursor-pointer"
+            onMouseEnter={() => setSelectedOption(item.title)}
+            onMouseLeave={() => setSelectedOption(null)}
+            className="group/nav relative flex items-center gap-5 text-sm cursor-pointer"
           >
             <span className="uppercase">{item.title}</span>
 
-            {/* Bottom border of the container itself */}
-            <div className="absolute left-0 bottom-0 h-px w-0 bg-slate-700 transition-all duration-300 group-hover:w-full"></div>
+            {/* Bottom border animation on nav item */}
+            <div className="absolute left-0 bottom-0 h-px w-0 bg-slate-700 transition-all duration-300 group-hover/nav:w-full" />
+
+            {/* Dropdown Menu */}
+            {selectedOption === item.title && item.options && (
+              <div className="absolute left-0 top-5 bg-white shadow-lg rounded-md p-4 flex flex-col gap-2">
+                {item.options.map((option, index) => (
+                  <div
+                    key={index}
+                    className="relative w-fit whitespace-nowrap cursor-pointer group/option"
+                  >
+                    <span>{option.title}</span>
+
+                    {/* Bottom border animation on submenu option */}
+                    <span className="absolute left-0 bottom-0 h-px w-0 bg-slate-700 transition-all duration-300 group-hover/option:w-full" />
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       </div>
