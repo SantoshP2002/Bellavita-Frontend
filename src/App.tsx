b539d7fab9f24cbcf5b13d959e3206183823f324
SantoshP2002 <santoshpawar96k@gmail.com>
2025-08-20 21:12:24 +0530

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import "./styles/global.style.css"
+import { RouterProvider } from "react-router-dom";
+import "./styles/global.style.css";
+import router from "./routes";
 function App() {
-  return <div className="bg-red-400">santosh</div>
+  return (
+    <div className="max-w-dvw max-h-dvh min-w-dvw min-h-dvh w-full h-full overflow-y-scroll bg-primary-inverted text-primary">
+      <div className="max-w-[1920px] w-full h-full mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
