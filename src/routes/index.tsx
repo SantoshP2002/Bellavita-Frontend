eed51499e744874dcbd46f6e4379a1d3dd81cf32
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-08 13:51:48 +0530

    Refactor admin routes to remove AdminDashboard and integrate AdminLayout with Dashboard, Users, Products, and Orders components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,27 +3,38 @@ import Main from "../pages/main";
 import Home from "../pages/home";
 import RegisterRedirect from "./RegisterRedirect";
 import LoginRedirect from "./LoginRedirect";
-import AdminDashboard from "../pages/admin/AdminDashboard";
 import PrivateRoute from "./PrivateRoute";
+
+// ADMIN PAGES
+import AdminLayout from "../pages/admin/AdminLayout";
+import Dashboard from "../pages/admin/children/Dashboard";
+import Users from "../pages/admin/children/Users";
+import Products from "../pages/admin/children/Products";
+import Orders from "../pages/admin/children/Orders";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: "admin",
+    element: (
+      <PrivateRoute>
+        <AdminLayout />
+      </PrivateRoute>
+    ),
+
     children: [
-      { index: true, element: <Home /> },
-      {
-        path: "/admin",
-        element: (
-          <PrivateRoute>
-            <AdminDashboard />
-          </PrivateRoute>
-        ),
-      },
+      { index: true, element: <Dashboard /> },
+      { path: "users", element: <Users /> },
+      { path: "products", element: <Products /> },
+      { path: "orders", element: <Orders /> },
     ],
   },
-  { path: "/login", element: <LoginRedirect /> },
-  { path: "/register", element: <RegisterRedirect /> },
+  { path: "login", element: <LoginRedirect /> },
+  { path: "register", element: <RegisterRedirect /> },
 ]);
 
 export default router;
