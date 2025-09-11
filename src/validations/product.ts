d74945bdb21041ab1edafeda85bdce677d3d347b
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-11 13:53:53 +0530

    Enhance uploadProductSchema validation to include sellingPrice, description, category, and productImages requirements

--- src/validations/product.ts
+++ src/validations/product.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { ALLOWED_IMAGE_TYPES, MAX_IMAGE_FILE_SIZE } from "../constants";
 
 export const uploadProductSchema = z.object({
   title: z
@@ -13,4 +14,29 @@ export const uploadProductSchema = z.object({
     .number({ error: "Price is required" })
     .nonnegative("Price must be a positive number")
     .min(1, { message: "Minimum price should be 1" }),
+  sellingPrice: z
+    .number({ error: "Selling price is required" })
+    .nonnegative("Selling price must be a positive number")
+    .min(1, { message: "Minimum Selling price should be 1" }),
+  description: z
+    .string({ error: "Description must be string" })
+    .nonempty("Description is required")
+    .min(1, { message: "Description must have minimum 2 characters long" }),
+  category: z
+    .string({ error: "Category must be string" })
+    .nonempty("Category is required"),
+  productImages: z
+    .array(
+      z
+        .custom<File>((file) => file instanceof File, {
+          message: "Invalid file",
+        })
+        .refine((file) => file.size <= MAX_IMAGE_FILE_SIZE, {
+          message: "Image not allowed more than 2MB",
+        })
+        .refine((file) => ALLOWED_IMAGE_TYPES.includes(file.type), {
+          message: "Invalid file type",
+        })
+    )
+    .min(1, "At least one image is required"),
 });
