399ed3be4a9cd19867a8f89421f17edcca423ade
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-12 13:59:09 +0530

    refactor: enhance create_product function with token authorization and improve error handling

--- src/api/products/api.ts
+++ src/api/products/api.ts
@@ -1,17 +1,36 @@
-import { AxiosError } from "axios"
-import api from "../axios.instance"
-import { apiRoutes } from "../routes"
+import { AxiosError } from "axios";
+import api from "../axios.instance";
+import { apiRoutes } from "../routes";
+import { getUserToken } from "../../utils";
 
 export const create_product = async (data: FormData) => {
-    try {
-        const { method, url } = apiRoutes.products.createProduct
-        const response = await api.request({ method, url, data })
-        return response.data
-    } catch (error) {
-        if (error instanceof AxiosError) {
-              // If it's an Axios error
-              throw error?.response?.data?.message || "API Error occurred";
-        }
-        throw "Something went wrong!"; // For non-Axios errors
+  try {
+    const token = getUserToken();
+    const { method, url } = apiRoutes.products.createProduct;
+    const response = await api.request({
+      method,
+      url,
+      data,
+      headers: { Authorization: token },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
     }
-}
+    throw "Something went wrong!";
+  }
+};
+
+export const getAll_Products = async () => {
+  try {
+    const { method, url } = apiRoutes.products.getAllProducts;
+    const response = await api.request({ method, url });
+    return response.data.product;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
