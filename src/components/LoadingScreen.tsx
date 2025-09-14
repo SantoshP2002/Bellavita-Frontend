9ce1c7d8671dcb9c8d38553f54423f2a8255a322
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-14 09:32:51 +0530

    Loading SCreen Updated

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,7 @@
 const LoadingScreen = () => {
   return (
-    <div className="h-dvh w-dvw flex flex-col items-center justify-center bg-white">
-      <div className="h-12 w-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin mb-4 shadow-lg shadow-indigo-300/50"></div>
+    <div className="h-dvh w-dvw flex flex-col items-center justify-center bg-white fixed inset-0 z-[100]">
+      <div className="h-12 w-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin mb-4 shadow-lg shadow-indigo-300/50" />
       <p className="text-lg font-semibold text-gray-700 animate-bounce">
         Loading...
       </p>
