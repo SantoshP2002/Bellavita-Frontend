6a15ea46068a376a04ccc35cd20e33cc1fa1089e
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-23 17:01:13 +0530

    update useAddToCart to use TProductCart type and add useUpdateQuantityCartProduct

--- src/api/cart/service.ts
+++ src/api/cart/service.ts
@@ -1,11 +1,15 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import type { TCart } from "../../types";
-import { add_to_cart, get_user_cart } from "./api";
+import type { TProductCart } from "../../types";
+import {
+  add_to_cart,
+  get_user_cart,
+  update_cart_product_quantity,
+} from "./api";
 import { toast } from "react-toastify";
 
 export const useAddToCart = () => {
   return useMutation({
-    mutationFn: (data: TCart) => add_to_cart(data),
+    mutationFn: (data: TProductCart) => add_to_cart(data),
     onSuccess: (data) => {
       toast.success(data?.message || "Product added to cart successfully!");
     },
@@ -21,5 +25,17 @@ export const useGetUserCart = () => {
     queryFn: () => get_user_cart(),
     enabled: true,
     refetchOnWindowFocus: false,
+  });
+};
+
+export const useUpdateQuantityCartProduct = () => {
+  return useMutation({
+    mutationKey: ["update_quantity_cart_product"],
+    mutationFn: ({ _id, quantity }: Pick<TProductCart, "_id" | "quantity">) =>
+      update_cart_product_quantity(_id, quantity),
+
+    onError: (error) => {
+      toast.error(typeof error === "string" ? error : "Something went wrong!");
+    },
   });
 };
