bd97ca7ea157a03717c496eab5c28b36f6d11f3d
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-24 14:19:49 +0530

    none

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -3,12 +3,7 @@ import CartProducts from "../../components/cartProducts";
 import Footer from "../../components/footer";
 
 const Cart = () => {
-  // Subtotal calculation
-  // const subtotal = cartItems.reduce(
-  //   (acc, item) => acc + item.sellingPrice * item.quantity,
-  //   0
-  // );
-
+  
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       {/* Main Section */}
