807b613ad823404b01423d84ccff348607ec647f
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-25 16:17:17 +0530

    fix: update HomeCarousel to accept images as props for improved display

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,19 @@
 import HomeCarousel from "../../components/homeCarousel";
 import HomeProducts from "../../components/HomeProducts";
+import { carouselImages } from "../../constants";
 
 const Home = () => {
   return (
     <div className="w-full max-w-7xl mx-auto">
       {/*Home Carousel */}
-      <HomeCarousel />
+      <HomeCarousel key={"top"} images={carouselImages}  needDots={true} />
       {/* ✅ Horizontal Scroll HomeProduct Section */}
       <HomeProducts />
+      <HomeCarousel key={"bottom"}
+        images={[
+          "https://bellavitaorganic.com/cdn/shop/files/Offer-Banner-2-UPB.webp?v=1727436765&width=1920",
+        ]}
+      />
     </div>
   );
 };
