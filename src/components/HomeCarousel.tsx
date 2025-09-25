41ab9cad75cadc1343ce7d52de35e14c5faad097
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-25 16:15:48 +0530

    fix: refactor HomeCarousel to accept images as props and improve layout

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from "react";
 
-const images = [
-  "https://bellavitaorganic.com/cdn/shop/files/Summer-Banner-1920x720.webp?v=1745321335&width=1920",
-  "https://bellavitaorganic.com/cdn/shop/articles/Perfect_Perfume_Gift_Sets_for_Any_Occasion.jpg?v=1684929262&width=1500",
-  "https://bellavitaorganic.com/cdn/shop/files/Offer-Banner-2-UPB.webp?v=1727436765&width=1920",
-];
-
-const HomeCarousel = () => {
+const HomeCarousel = ({
+  images,
+  needDots,
+}: {
+  images: string[];
+  needDots?: boolean;
+}) => {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
+    if (images.length <= 1) return;
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % images.length);
     }, 3000);
     return () => clearInterval(interval);
-  }, []);
-    
-    
+  }, [images.length]);
+
   return (
     <div>
-      <div className="mt-10 relative overflow-hidden h-[400px] rounded-2xl shadow-lg">
+      <div className="mt-5 relative overflow-hidden h-full">
         {images.map((img, index) => (
           <img
             key={index}
             src={img}
             alt={`Slide ${index + 1}`}
-            className={`w-full h-full object-cover transition-opacity duration-1000 ${
+            className={`w-full h-full object-cover transition-opacity duration-2000 ${
               index === current
                 ? "opacity-100"
                 : "opacity-0 absolute top-0 left-0"
@@ -35,24 +35,19 @@ const HomeCarousel = () => {
       </div>
 
       {/* Dots */}
-      <div className="flex justify-center mt-4 gap-2 mb-6">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrent(index)}
-            className={`h-3 w-3 rounded-full transition-colors duration-300 ${
-              index === current ? "bg-blue-600" : "bg-gray-300"
-            }`}
-          />
-        ))}
-          </div>
-          
-      {/* Content Header */}
-      <div className="flex justify-center items-center gap-4 mt-8">
-        <h1 className="text-2xl font-bold text-black">BESTSELLER</h1>
-        <span className="text-gray-400">|</span>
-        <h1 className="text-2xl font-semibold text-gray-500">NEW ARRIVALS</h1>
-      </div>
+      {needDots && (
+        <div className="flex justify-center mt-4 gap-2">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrent(index)}
+              className={`h-3 w-3 rounded-full transition-colors duration-300 ${
+                index === current ? "bg-blue-600" : "bg-gray-300"
+              }`}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
