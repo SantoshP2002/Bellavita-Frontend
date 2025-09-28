059fc93a67c49b1c0caa76491ada7512cbf2f5d4
SantoshP2002 <santoshpawar96k@gmail.com>
2025-09-28 14:28:03 +0530

    Added New Component in Home Page (QuizProducts)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,24 @@
 import HomeCarousel from "../../components/homeCarousel";
 import HomeProducts from "../../components/HomeProducts";
 import LuxuryCategory from "../../components/LuxuryCategory";
+import QuizProducts from "../../components/QuizProducts";
 import { carouselImages } from "../../constants";
 
 const Home = () => {
   return (
     <div className="w-full max-w-7xl mx-auto">
       {/*Home Carousel */}
-      <HomeCarousel key={"top"} images={carouselImages}  needDots={true} />
+      <HomeCarousel key={"top"} images={carouselImages} needDots={true} />
       {/* ✅ Horizontal Scroll HomeProduct Section */}
       <HomeProducts />
-      <HomeCarousel key={"bottom"}
+      <HomeCarousel
+        key={"bottom"}
         images={[
           "https://bellavitaorganic.com/cdn/shop/files/Offer-Banner-2-UPB.webp?v=1727436765&width=1920",
         ]}
       />
-      <LuxuryCategory/>
+      <LuxuryCategory />
+      <QuizProducts />
     </div>
   );
 };
