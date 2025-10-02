54f64f01159a08d81760c824a822136c8026d17b
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-02 20:44:02 +0530

    create useUpdateAddress service of address

--- src/api/address/service.ts
+++ src/api/address/service.ts
@@ -1,9 +1,9 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { add_address, get_address } from "./api";
+import { add_address, get_address, update_address } from "./api";
 import { toast } from "react-toastify";
 
 export const useAddAddress = () => {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["add_address"],
     mutationFn: add_address,
@@ -22,5 +22,18 @@ export const useGetAddress = () => {
     queryFn: get_address,
     enabled: true,
     refetchOnWindowFocus: false,
+  });
+};
+
+export const useUpdateAddress = () => {
+  return useMutation({
+    mutationKey: ["update_address"],
+    mutationFn: update_address,
+    onSuccess: (data) => {
+      toast.success(data?.message || "Address updated successfully!");
+    },
+    onError: (error) => {
+      toast.error(typeof error === "string" ? error : "Something went wrong!");
+    },
   });
 };
