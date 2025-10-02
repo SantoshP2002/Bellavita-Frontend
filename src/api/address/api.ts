1d392c3d89dd21be85da95a29d8593639083c7b6
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-02 20:43:23 +0530

    create update_address api for address

--- src/api/address/api.ts
+++ src/api/address/api.ts
@@ -2,8 +2,9 @@ import { AxiosError } from "axios";
 import { getUserToken } from "../../utils";
 import { apiRoutes } from "../routes";
 import api from "../axios.instance";
-import type {  IBaseAddress } from "../../types";
+import type {  IAddress, IBaseAddress } from "../../types";
 
+// add address
 export const add_address = async (addressData: IBaseAddress) => {
   try {
     const token = getUserToken();
@@ -24,6 +25,7 @@ export const add_address = async (addressData: IBaseAddress) => {
   }
 };
 
+// get address
 export const get_address = async () => {
   try {
     const token = getUserToken();
@@ -31,6 +33,26 @@ export const get_address = async () => {
     const response = await api.request({
       method,
       url,
+      headers: { Authorization: token },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
+
+// update address
+export const update_address = async (data: Partial<IAddress>) => {
+  try {
+    const token = getUserToken();
+    const { method, url } = apiRoutes.address.updateAddress;
+    const response = await api.request({
+      method,
+      url: `${url}/${data._id}`,
+      data,
       headers: { Authorization: token },
     });
     return response.data;
