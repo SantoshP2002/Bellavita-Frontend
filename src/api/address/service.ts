9ae75cf68f33f908470be71056925d6ae612d740
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-03 17:02:48 +0530

    create address in services of useDeleteAddress

--- src/api/address/service.ts
+++ src/api/address/service.ts
@@ -1,5 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { add_address, get_address, update_address } from "./api";
+import {
+  add_address,
+  delete_address,
+  get_address,
+  update_address,
+} from "./api";
 import { toast } from "react-toastify";
 
 export const useAddAddress = () => {
@@ -26,11 +31,30 @@ export const useGetAddress = () => {
 };
 
 export const useUpdateAddress = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["update_address"],
     mutationFn: update_address,
     onSuccess: (data) => {
       toast.success(data?.message || "Address updated successfully!");
+      queryClient.invalidateQueries({ queryKey: ["get_address"] });
+    },
+    onError: (error) => {
+      toast.error(typeof error === "string" ? error : "Something went wrong!");
+    },
+  });
+};
+
+export const useDeleteAddress = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["delete_address"],
+    mutationFn: delete_address,
+    onSuccess: (data) => {
+      toast.success(data?.message || "Address updated successfully!");
+      queryClient.invalidateQueries({ queryKey: ["get_address"] });
+
     },
     onError: (error) => {
       toast.error(typeof error === "string" ? error : "Something went wrong!");
