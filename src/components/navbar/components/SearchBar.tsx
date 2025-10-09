aa1961433a5b9ac071b2dd198c6ecc1aee352ea2
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-09 10:43:07 +0530

    css changes

--- src/components/navbar/components/SearchBar.tsx
+++ src/components/navbar/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
   return (
-    <div className="border-b border-black flex items-center gap-2 py-1 w-fit">
+    <div className="border-b border-black flex items-center gap-2 py-2 w-fit">
       <CiSearch className="w-5 h-5" />
       <input
         className="text-sm border-none outline-none focus:outline-none"
