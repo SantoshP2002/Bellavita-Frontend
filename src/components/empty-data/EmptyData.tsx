43660611f3a526844771f6e3eaeab98b87dbca64
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-17 12:05:46 +0530

    EmptyData component in changes of css

--- src/components/empty-data/EmptyData.tsx
+++ src/components/empty-data/EmptyData.tsx
@@ -10,7 +10,7 @@ const EmptyData = ({ className = "", content = "" }: EmptyDataProps) => {
     <div
       className={`flex flex-col items-center justify-center gap-2 p-4 ${className}`}
     >
-      <GoContainer className="w-12 h-12" />
+      <GoContainer className="w-12 h-12 text-shadow-sm" />
       <h3 className="text-center text-lg text-shadow-sm font-medium">
         {content}
       </h3>
