4176854e8ec60d53202e931f9b37e4b7ceecff14
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-26 11:29:29 +0530

    Added search funtionlaity

--- src/components/navbar/components/SearchBar.tsx
+++ src/components/navbar/components/SearchBar.tsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react";
 import { CiSearch } from "react-icons/ci";
+import { useDebounce } from "../../../hooks/useDebounce";
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    onSearch(debouncedSearch);
+  }, [debouncedSearch, onSearch]);
+
   return (
     <div className="border-b border-black flex items-center gap-2 py-2 w-fit">
       <CiSearch className="w-5 h-5" />
@@ -8,6 +17,8 @@ const SearchBar = () => {
         className="text-sm border-none outline-none focus:outline-none"
         type="text"
         placeholder="Search for perfumes..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
