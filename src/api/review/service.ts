20e24b9e52c0b371a10e8b5227e0db6ed05a56d7
SantoshP2002 <santoshpawar96k@gmail.com>
2025-10-26 11:16:24 +0530

    create services useCreateReviews and useGetReviewByProductId

---
+++ src/api/review/service.ts
@@ -0,0 +1,48 @@
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import usePathParams from "../../hooks/usePathParams";
+import { create_reviews, get_review_by_product_id } from "./api";
+
+// create review
+export const useCreateReviews = () => {
+  const { pathParams } = usePathParams();
+  const queryClient = useQueryClient();
+  const productId = pathParams.productId || "";
+
+  return useMutation({
+    mutationKey: ["create_reviews", productId],
+    mutationFn: (data: FormData) => create_reviews(data, productId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["get_reviews_by_product_id_infinite"],
+      });
+      queryClient.invalidateQueries({ queryKey: ["get_product_by_id"] });
+    },
+  });
+};
+
+// get review by product id
+export const useGetReviewByProductId = () => {
+  const { pathParams } = usePathParams();
+  const productId = pathParams.productId || "";
+
+  return useInfiniteQuery({
+    queryKey: ["get_reviews_by_product_id_infinite", productId],
+
+    // pageParam ka default value
+    initialPageParam: 1,
+    queryFn: async ({ pageParam }) =>
+      get_review_by_product_id(productId, pageParam, 10, false),
+
+    getNextPageParam: (lastPage) => {
+      const { pagination } = lastPage || {};
+      if (pagination?.hasNextPage) {
+        return pagination.currentPage + 1;
+      }
+      return undefined;
+    },
+  });
+};
