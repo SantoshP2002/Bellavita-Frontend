a11d5eef949526b75a527c3b7345cfccdcfc5fdc
SantoshP2002 <santoshpawar96k@gmail.com>
2025-11-04 18:53:42 +0530

    order api in create Get Admin Order

--- src/api/order/api.ts
+++ src/api/order/api.ts
@@ -61,3 +61,22 @@ export const get_order_By_Id = async (orderId: string) => {
     throw "Something went wrong!";
   }
 };
+
+// Get Admin Order
+export const get_admin_order = async () => {
+  try {
+    const token = getUserToken();
+    const { method, url } = apiRoutes.order.getAdminOrder;
+    const response = await api.request({
+      method,
+      url,
+      headers: { Authorization: token },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
