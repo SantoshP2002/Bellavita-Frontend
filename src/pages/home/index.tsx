53b20e0f372c454bd8331a04cef20fd8cfa718bf
SantoshP2002 <santoshpawar96k@gmail.com>
2025-11-14 09:36:42 +0530

    Home page in add ImageStrip, AutoScrollStrip and  HomeCarousel in add mobileimages on top and bottom both

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,34 +1,47 @@
+import AutoScrollStrip from "../../components/AutoScrollStrip";
 import BellamoodTech from "../../components/BellamoodTech";
 import CrazyDeals from "../../components/CrazyDeals";
 import CustomerCarousel from "../../components/CustomerCarousel";
 import HomeCarousel from "../../components/homeCarousel";
 import HomeProducts from "../../components/HomeProducts";
+import ImageStrip from "../../components/ImageStrip";
 import LuxuryCategory from "../../components/LuxuryCategory";
 import QuizProducts from "../../components/QuizProducts";
 import ShopByNotes from "../../components/ShopByNotes";
 import WhyBellavitaInfo from "../../components/WhyBellavitaInfo";
-import { carouselImages } from "../../constants";
+import { carouselImages, mobileCarouselImages } from "../../constants";
 
 const Home = () => {
   return (
     <div className="w-full max-w-7xl mx-auto">
       {/*Home Carousel */}
-      <HomeCarousel key={"top"} images={carouselImages} needDots={true} />
+      <HomeCarousel
+        key={"top"}
+        images={carouselImages}
+        mobileImages={mobileCarouselImages}
+        needDots={true}
+      />
       {/* ✅ Horizontal Scroll HomeProduct Section */}
       <HomeProducts />
       <HomeCarousel
-        key={"bottom"}
+        key="bottom"
         images={[
           "https://bellavitaorganic.com/cdn/shop/files/Offer-Banner-2-UPB.webp?v=1727436765&width=1920",
         ]}
+        mobileImages={[
+          "https://bellavitaorganic.com/cdn/shop/files/Offer-Mobile-_1_UPB-mobile.webp?v=1727436765&width=800",
+        ]}
       />
+
       <LuxuryCategory />
       <QuizProducts />
       <BellamoodTech />
       <CrazyDeals />
       <ShopByNotes />
       <WhyBellavitaInfo />
-      <CustomerCarousel/>
+      <CustomerCarousel />
+      <ImageStrip />
+      <AutoScrollStrip />
     </div>
   );
 };
