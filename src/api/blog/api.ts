3290d9745447abbbeeb84695633d17c36025f2c4
SantoshP2002 <santoshpawar96k@gmail.com>
2025-12-24 21:52:28 +0530

    create blog api in get_blog &  get_blog_By_Id

--- src/api/blog/api.ts
+++ src/api/blog/api.ts
@@ -21,3 +21,37 @@ export const create_blog = async (data: FormData) => {
     throw "Something went wrong!";
   }
 };
+
+// get Blog
+export const get_blog = async () => {
+  try {
+    const { method, url } = apiRoutes.blog.getBlog;
+    const response = await api.request({
+      method,
+      url,
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
+
+// get blog by Id
+export const get_blog_By_Id = async (id: string) => {
+  try {
+    const { method, url } = apiRoutes.blog.getBlogById;
+    const response = await api.request({
+      method,
+      url: `${url}/${id}`,
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
