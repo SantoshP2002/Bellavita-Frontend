c6221f948739b762e8f47ad38627a046322df665
SantoshP2002 <santoshpawar96k@gmail.com>
2025-12-27 20:00:25 +0530

    feat: enhance AllBlogs component with loading and error handling, add delete functionality

--- src/pages/admin/children/AllBlogs.tsx
+++ src/pages/admin/children/AllBlogs.tsx
@@ -1,20 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import { Button } from "../../../components/Button";
-import { useGetBlog } from "../../../api/blog/service";
+import { useDeleteBlogById, useGetBlog } from "../../../api/blog/service";
 import type { IBlog } from "../../../types";
+import LoadingScreen from "../../../components/LoadingScreen";
+import EmptyData from "../../../components/empty-data/EmptyData";
 
 const AllBlogs = () => {
   const navigate = useNavigate();
 
+  const deleteProductQuery = useDeleteBlogById();
   const { data, isLoading, isError, error } = useGetBlog();
 
-  const blogs: IBlog[] = data?.blog || [];
+  const blog: IBlog[] = data?.blog || [];
 
-  console.log("BLOGS11", blogs);
-
-  if (isLoading) return <p>Loading blogs...</p>;
-  if (isError) return <p>{String(error)}</p>;
+  const handleDelete = (id: string) => {
+    if (window.confirm("Are you sure you want to delete this product?")) {
+      deleteProductQuery.mutate(id);
+    }
+  };
 
   return (
     <div>
@@ -38,26 +42,73 @@ const AllBlogs = () => {
       <hr className="my-4" />
 
       {/* ALL BLOGS LIST */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogs.map((blog) => (
-          <div key={blog._id} className="border rounded-lg p-4 shadow-sm">
-            <img
-              src={blog.image}
-              alt={blog.title}
-              className="w-full h-40 object-cover rounded-md"
-            />
-            <h4 className="text-lg font-semibold mt-3">{blog.title}</h4>
-            <p>
-              {new Date(blog.createdAt).toLocaleDateString("en-IN", {
-                day: "2-digit",
-                month: "long",
-                year: "numeric",
-              })}
-            </p>
+      <div>
+        {isLoading && (
+          <p>
+            <LoadingScreen />
+          </p>
+        )}
+        {isError && (
+          <p className="text-red-500">
+            Failed to load products {String(error)}
+          </p>
+        )}
 
-            <p className="text-sm text-gray-600 mt-1">{blog.description}</p>
+        {blog.length === 0 ? (
+          <p className="text-black mt-4 text-xl font-semibold">
+            <EmptyData content="No Blog Found" />
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {blog.map((blog) => (
+              <div
+                key={blog._id}
+                className="bg-white rounded-lg p-4 hover:shadow-sm"
+              >
+                <img
+                  src={blog.image}
+                  alt={blog.title}
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h4 className="text-lg font-semibold mt-3 line-clamp-2">
+                  {blog.title}
+                </h4>
+                <p>
+                  {new Date(blog.createdAt).toLocaleDateString("en-IN", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+
+                <p className="text-sm text-gray-600 mt-1 line-clamp-5">
+                  {blog.description}
+                </p>
+
+                <div className="flex flex-row gap-2">
+                  {/* Update Button  */}
+                  <Button
+                    content="Update"
+                    className="mt-4 w-full bg-black text-white text-sm py-2 rounded-lg hover:bg-gray-800 transition-colors duration-300"
+                    buttonProps={{
+                      onClick: () => navigate(`/admin/blog/edit/${blog._id}`),
+                    }}
+                  />
+
+                  {/* Delete Button  */}
+                  <Button
+                    pattern="secondary"
+                    content="Delete"
+                    className="mt-4 w-full bg-black text-white text-sm py-2 rounded-lg hover:bg-gray-800 transition-colors duration-300"
+                    buttonProps={{
+                      onClick: () => handleDelete(blog._id),
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
