eb9fbe79514aa855ac2cc0747fa8c2a4c038d6f0
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-01 15:00:03 +0530

    button position change only in AdminHeaders page

--- src/pages/admin/components/AdminHeader.tsx
+++ src/pages/admin/components/AdminHeader.tsx
@@ -1,4 +1,4 @@
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaLongArrowAltRight } from "react-icons/fa";
 import { Button } from "../../../components/Button";
 import { useNavigate } from "react-router-dom";
 
@@ -9,36 +9,45 @@ type AdminHeadersProps = {
 const AdminHeader = ({ onMenuClick }: AdminHeadersProps) => {
   const navigate = useNavigate();
   return (
-    <header className="flex justify-between items-center p-4 border-b">
-      {/* Left Section */}
-      <div className="flex items-center gap-3">
-        <Button
-          content={<FaBars size={22} />}
-          buttonProps={{
-            onClick: onMenuClick,
-            className: "md:hidden text-gray-700",
-          }}
-        />
-        <h1 className="text-2xl font-bold text-gray-700">Dashboard</h1>
-        <Button
-          content="home/"
-          className="underline text-indigo-500"
-          buttonProps={{
-            onClick: () => navigate("/"),
-          }}
-        />
-      </div>
+    <div>
+      <Button
+        content="Back To Home"
+        icons={{
+          right: <FaLongArrowAltRight />,
+        }}
+        pattern="secondary"
+        className=" text-indigo-500 w-50!"
+        buttonProps={{
+          onClick: () => navigate("/"),
+        }}
+      />
+      <header className="flex justify-between items-center p-4 border-b">
+        {/* Left Section */}
 
-      {/* Right Section */}
-      <div className="flex items-center gap-3">
-        <span className="text-gray-600 hidden sm:block">Welcome, Admin 👋</span>
-        <img
-          src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-118143566.jpg"
-          alt="Admin"
-          className="w-10 h-10 rounded-full border"
-        />
-      </div>
-    </header>
+        <div className="flex items-center gap-3">
+          <Button
+            content={<FaBars size={22} />}
+            buttonProps={{
+              onClick: onMenuClick,
+              className: "md:hidden text-gray-700",
+            }}
+          />
+          <h1 className="text-2xl font-bold text-gray-700">Dashboard</h1>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex items-center gap-3">
+          <span className="text-gray-600 hidden sm:block">
+            Welcome, Admin 👋
+          </span>
+          <img
+            src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-118143566.jpg"
+            alt="Admin"
+            className="w-10 h-10 rounded-full"
+          />
+        </div>
+      </header>
+    </div>
   );
 };
 
