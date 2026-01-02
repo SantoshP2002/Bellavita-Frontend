6bed403f36549146b6c178627c660efd7aab44bc
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-03 00:59:40 +0530

    user api in add update_user

--- src/api/user/api.ts
+++ src/api/user/api.ts
@@ -2,6 +2,7 @@ import { AxiosError } from "axios";
 import api from "../axios.instance";
 import { apiRoutes } from "../routes";
 import { getUserToken } from "../../utils";
+import type { IUser } from "../../types";
 
 export const get_user = async () => {
   try {
@@ -10,14 +11,34 @@ export const get_user = async () => {
     const response = await api.request({
       method,
       url,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error?.response?.data?.message || "API Error occurred";
+    }
+    throw "Something went wrong!";
+  }
+};
+
+export const update_user = async (data: IUser) => {
+  try {
+    const token = getUserToken();
+    const { method, url } = apiRoutes.user.updateUser;
+    const response = await api.request({
+      method,
+      data,
+      url,
       headers: { Authorization: token },
     });
     return response.data;
   } catch (error) {
     if (error instanceof AxiosError) {
-      // If it's an Axios error
       throw error?.response?.data?.message || "API Error occurred";
     }
-    throw "Something went wrong!"; // For non-Axios errors
+    throw "Something went wrong!";
   }
 };
