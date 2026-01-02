bf1e545f26bf262998f33ceb19d7d96a205693a6
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-03 00:56:45 +0530

    user api in add useUpdateUser

--- src/api/user/service.ts
+++ src/api/user/service.ts
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import { get_user } from "./api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { get_user, update_user } from "./api";
+import { toast } from "react-toastify";
 
 export const useGetUser = () => {
   return useQuery({
@@ -7,6 +8,19 @@ export const useGetUser = () => {
     queryFn: () => get_user(),
     enabled: true,
     refetchOnWindowFocus: false,
-    retry: false
+    retry: false,
+  });
+};
+
+export const useUpdateUser = () => {
+  return useMutation({
+    mutationKey: ["update_user"],
+    mutationFn: update_user,
+    onSuccess: (data) => {
+      toast.success(data?.message || "Product updated successfully!");
+    },
+    onError: (error) => {
+      toast.error(typeof error === "string" ? error : "Something went wrong!");
+    },
   });
 };
