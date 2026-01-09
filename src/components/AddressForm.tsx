0d9bd5b9e09ea1aab6fc50edc8d3f6c2b9fceea5
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-09 16:30:41 +0530

    AddressForm component in input and bustton css changes

--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -67,69 +67,80 @@ const AddressForm = ({
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={`${className}`}>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
         <Input
           label="First Name"
+          className="border-b-4 border-r-4"
           register={register("firstName")}
           inputProps={{ placeholder: "Enter First Name" }}
         />
         <Input
           label="Last Name"
+          className="border-b-4 border-r-4"
           register={register("lastName")}
           inputProps={{ placeholder: "Enter Last Name" }}
         />
         <div className="col-span-2">
           <Input
             label="Email"
+            className="border-b-4 border-r-4"
             register={register("email")}
             inputProps={{ placeholder: "Enter Email", type: "email" }}
           />
         </div>
         <Input
           label="Phone Number"
+          className="border-b-4 border-r-4"
           register={register("phoneNumber")}
-          inputProps={{ placeholder: "Enter Phone Number" }}
+          inputProps={{ placeholder: "Enter Phone Number", type: "number" }}
         />
         <Input
           label="Alt. Phone Number"
+          className="border-b-4 border-r-4"
           register={register("altPhoneNumber")}
-          inputProps={{ placeholder: "Enter Alternative Number" }}
+          inputProps={{ placeholder: "Enter Alt. Number", type:"number" }}
         />
         <Input
           label="Address"
+          className="border-b-4 border-r-4"
           register={register("address")}
           inputProps={{ placeholder: "Enter Address" }}
         />
         <Input
           label="Landmark"
+          className="border-b-4 border-r-4"
           register={register("landmark")}
           inputProps={{ placeholder: "Enter Landmark" }}
         />
         <Input
           label="City"
+          className="border-b-4 border-r-4"
           register={register("city")}
           inputProps={{ placeholder: "Enter City" }}
         />
         <Input
           label="Pincode"
+          className="border-b-4 border-r-4"
           register={register("pinCode")}
-          inputProps={{ placeholder: "Enter PinCode", type: "number" }}
+          inputProps={{ placeholder: "Enter Pin-Code", type: "number" }}
         />
         <Input
           label="State"
+          className="border-b-4 border-r-4"
           register={register("state")}
           inputProps={{ placeholder: "Enter State" }}
         />
         <Input
           label="Country"
+          className="border-b-4 border-r-4"
           register={register("country")}
           inputProps={{ placeholder: "Enter Country" }}
         />
       </div>
       <Button
         content={isPending ? "Saving..." : "Save Address"}
-        pattern="secondary"
-        className="mt-6"
+        pattern="outline"
+        className="w-50! mt-4 bg-white text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 shadow-[4px_4px_0_0_#000] transition-all duration-200 ease-out"
         buttonProps={{ type: "submit", disabled: isPending }}
       />
     </form>
