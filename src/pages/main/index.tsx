32b6ce00f7238eadbd20ee6754c4cc80e4df3b67
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-10 14:31:49 +0530

    page/main in index file in add ScrollToTop component

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const Main = () => {
-
   return (
     <div className="w-full h-full flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <main className="grow flex-1">
         <Outlet />
