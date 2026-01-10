5572533ad9b66978a3173b84a9e6acd71548a799
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-10 19:08:14 +0530

    cart api in changes useAddToCart and useDeleteCartProduct in added a queryClient

--- src/api/cart/service.ts
+++ src/api/cart/service.ts
@@ -1,18 +1,23 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { TProductCart } from "../../types";
 import {
   add_to_cart,
   delete_cart_Product,
   get_user_cart,
   update_cart_product_quantity,
-} from "./api"
+} from "./api";
 import toast from "react-hot-toast";
+import { useUserStore } from "../../store/user";
 
 export const useAddToCart = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (id: string) => add_to_cart(id),
     onSuccess: (data) => {
       toast.success(data?.message || "Product added to cart successfully!");
+
+      queryClient.invalidateQueries({ queryKey: ["get_user_cart"] });
     },
     onError: (error) => {
       toast.error(typeof error === "string" ? error : "Something went wrong!");
@@ -21,11 +26,13 @@ export const useAddToCart = () => {
 };
 
 export const useGetUserCart = () => {
+  const { isLoggedIn } = useUserStore();
   return useQuery({
     queryKey: ["get_user_cart"],
     queryFn: () => get_user_cart(),
-    enabled: true,
+    enabled: isLoggedIn,
     refetchOnWindowFocus: false,
+    staleTime: 0,
   });
 };
 
@@ -42,11 +49,14 @@ export const useUpdateQuantityCartProduct = () => {
 };
 
 export const useDeleteCartProduct = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (id: string) => delete_cart_Product(id),
 
     onSuccess: (data) => {
       toast.success(data?.message || "Product deleted to cart successfully!");
+
+      queryClient.invalidateQueries({ queryKey: ["get_user_cart"] });
     },
     onError: (error) => {
       toast.error(typeof error === "string" ? error : "Something went wrong!");
