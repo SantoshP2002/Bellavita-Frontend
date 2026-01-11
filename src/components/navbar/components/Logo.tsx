936e5d44db3f49ca3fc2387e2d971f6e6f3ff7f6
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-11 20:46:35 +0530

    navbar Logo component remove unused code

--- src/components/navbar/components/Logo.tsx
+++ src/components/navbar/components/Logo.tsx
@@ -1,31 +1,20 @@
-import type React from "react";
+import { Link } from "react-router-dom";
 
-interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
-  onClick?: () => void;
-}
-
-const Logo: React.FC<LogoProps> = ({ onClick }) => {
-  return (
-    <div onClick={onClick}>
-      {/* DESKTOP LOGO */}
-      <img
-        className="hidden md:block h-13"
-        src="/assets/images/download.png"
-        alt="Logo bellavita"
-      />
-
-      {/* MOBILE LOGO */}
-      <img
-        className="block md:hidden h-15 cursor-pointer"
-        src="https://t4.ftcdn.net/jpg/13/06/18/43/240_F_1306184377_YCytm1yWub6o610ECqPinPxo54hX0vKJ.jpg"
-        alt="Logo bellavita mobile"
-      />
-    </div>
-  );
-};
+const Logo = () => (
+  <Link to="/">
+    {/* DESKTOP LOGO */}
+    <img
+      className="hidden md:block h-13"
+      src="/assets/images/download.png"
+      alt="Logo bellavita"
+    />
+    {/* MOBILE LOGO */}
+    <img
+      className="block md:hidden h-15 cursor-pointer"
+      src="https://t4.ftcdn.net/jpg/13/06/18/43/240_F_1306184377_YCytm1yWub6o610ECqPinPxo54hX0vKJ.jpg"
+      alt="Logo bellavita mobile"
+    />
+  </Link>
+);
 
 export default Logo;
-
-{
-  /* <img src="/logo.png" alt="Logo" className="h-8 w-8" />; */
-}
