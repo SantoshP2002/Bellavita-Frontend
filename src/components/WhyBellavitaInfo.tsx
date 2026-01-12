ffb619b7788f9a16344f2cdeb969717c146829e5
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-12 14:56:25 +0530

    WhyBellavitaInfo component fix and make responsive

--- src/components/WhyBellavitaInfo.tsx
+++ src/components/WhyBellavitaInfo.tsx
@@ -1,31 +1,4 @@
-const whyBellavitaData = [
-  {
-    title: "CRUELTY FREE",
-    description:
-      "Kindness in every bottle: Our commitment to cruelty-free Products.",
-    image:
-      "https://static.vecteezy.com/system/resources/previews/036/363/698/non_2x/cruelty-free-icon-eco-friendly-natural-cruelty-free-and-gentle-on-skin-icon-free-vector.jpg",
-  },
-  {
-    title: "FRAGRANCE FORWARD",
-    description: "Luxurious & imported perfume oils in every product.",
-    image:
-      "https://c8.alamy.com/comp/2PP9RTK/fragrance-free-vector-line-circle-stamp-no-fragrances-for-cosmetics-packaging-label-2PP9RTK.jpg",
-  },
-  {
-    title: "AFFORDABLE LUXURY",
-    description: "Offering Premium Quality and Elegance at a Reasonable Price.",
-    image:
-      "https://static.vecteezy.com/system/resources/previews/028/307/994/non_2x/wallet-icon-simple-outline-style-affordable-investment-money-cash-dollar-bill-payment-business-finance-concept-thin-line-symbol-isolated-on-white-background-svg-vector.jpg",
-  },
-  {
-    title: "GENDER NEUTRAL",
-    description:
-      "Elevate your self-care routine with bath, body and personal care for all.",
-    image:
-      "https://www.shutterstock.com/image-vector/gender-neutral-symbol-thin-line-260nw-1614585991.jpg",
-  },
-];
+import { whyBellavitaData } from "../constants";
 
 const WhyBellavitaInfo = () => {
   return (
@@ -42,7 +15,7 @@ const WhyBellavitaInfo = () => {
             <img
               src={item.image}
               alt={item.title}
-              className="w-20 h-20 sm:w-24 sm:h-24 object-contain rounded-full bg-white shadow-sm mb-4 hover:scale-105 transition-transform duration-300"
+              className="w-15 h-15 sm:w-24 sm:h-24 object-contain rounded-full bg-white shadow-sm mb-4 hover:scale-105 transition-transform duration-300"
             />
             <h3 className="text-base md:text-lg text-gray-800 uppercase font-medium">
               {item.title}
@@ -62,7 +35,7 @@ const WhyBellavitaInfo = () => {
           alt="Bellavita banner desktop"
           className="w-full h-auto object-cover hidden sm:block"
         />
-    
+
         {/* 📱 Mobile image */}
         <img
           src="https://bellavitaorganic.com/cdn/shop/files/BVO_Mobile.webp?v=1751025395&width=800"
