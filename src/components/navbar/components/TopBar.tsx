85874c9e189de010ee40508c4bfa8d70cd64e651
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-12 14:57:08 +0530

    navbar Topbar component fix

--- src/components/navbar/components/TopBar.tsx
+++ src/components/navbar/components/TopBar.tsx
@@ -20,13 +20,13 @@ const TopBar = () => {
                   setActiveCategory(item.value);
                   navigate(`/products?category=${item.value}`);
                 }}
-                className={`flex items-center gap-2 whitespace-nowrap text-xs font-medium px-6 py-2 rounded border cursor-pointer ${
+                className={`flex items-center gap-2 whitespace-nowrap text-xs font-medium px-6 py-2 rounded cursor-pointer ${
                   activeCategory === item.value
                     ? "bg-gray-900 text-white border-black"
                     : "bg-gray-100 text-gray-900 border-gray-300"
                 } `}
               >
-                {categoryIconMap[item.name] && categoryIconMap[item.name]}{" "}
+                {categoryIconMap[item.value] && categoryIconMap[item.value]}{" "}
                 {item.name}
               </span>
             ))}
