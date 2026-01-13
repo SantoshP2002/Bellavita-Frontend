2ebcc821a448835bdffd26ada940b42fc2966493
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-14 00:15:12 +0530

    Login page in changes off dark mode css and responsive

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { FaArrowRight, FaLock, FaRegUser } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+import { FaLock, FaRegUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import type { TBaseUser } from "../../types";
 import { loginSchema } from "../../validations/auth";
 import type z from "zod";
@@ -11,7 +11,6 @@ import Input from "../../components/Input";
 import { VITE_BACKEND_URI } from "../../env";
 
 const Login = () => {
-  const navigate = useNavigate();
   const { mutateAsync, isPending } = useLoginUser();
 
   const {
@@ -31,7 +30,7 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-dvh w-dvw flex flex-col gap-5 items-center justify-center px-4">
+    <div className="min-h-dvh w-dvw flex flex-col gap-5 items-center justify-center px-4 dark:bg-black">
       {/* MAIN CARD */}
       <div className="flex w-full max-w-5xl overflow-hidden">
         {/* LEFT IMAGE SECTION */}
@@ -49,11 +48,7 @@ const Login = () => {
             className="w-full max-w-md mx-auto"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <h1
-              className="text-3xl font-bold text-center mb-6 
-               bg-gradient-to-r from-blue-400 to-purple-700 
-               text-transparent bg-clip-text"
-            >
+            <h1 className="text-3xl font-bold text-center mb-6 bg-gradient-to-r from-blue-400 to-purple-700 text-transparent bg-clip-text">
               LOGIN
             </h1>
 
@@ -61,11 +56,11 @@ const Login = () => {
             <div className="relative mb-4">
               <Input
                 label="Email"
-                className="w-full px-4 py-3 pl-9 border-b-4 rounded-lg focus:outline-none focus:ring-indigo-400 text-sm"
+                className="border-b-4 border-r-4 px-5 lg:px-8 bg-white text-black border-black dark:bg-black dark:text-white dark:border-gray-700"
                 register={register("email")}
                 inputProps={{ placeholder: "Enter your email", name: "email" }}
               />
-              <FaRegUser className="absolute top-4 left-3 text-gray-500" />
+              <FaRegUser className="absolute size-3 lg:size-4 top-3 lg:top-4 left-3 text-gray-500" />
               {errors.email && (
                 <p className="text-red-500 text-xs mt-1">
                   {errors.email.message}
@@ -77,11 +72,11 @@ const Login = () => {
             <div className="relative mb-4">
               <Input
                 label="Password"
-                className="w-full px-4 py-3 pl-9 border-b-4 rounded-lg focus:outline-none focus:ring-indigo-400 text-sm"
+                className="border-b-4 border-r-4 px-5 lg:px-8 bg-white text-black border-black dark:bg-black dark:text-white dark:border-gray-700"
                 register={register("password")}
                 inputProps={{ placeholder: "Enter password", name: "password" }}
               />
-              <FaLock className="absolute top-4 left-3 text-gray-500" />
+              <FaLock className="absolute size-3 lg:size-4 top-3 lg:top-4 left-3 text-gray-500" />
               {errors.password && (
                 <p className="text-red-500 text-xs mt-1">
                   {errors.password.message}
@@ -91,7 +86,10 @@ const Login = () => {
 
             {/* Forgot Password */}
             <div className="text-right mb-4">
-              <Link to="/" className="text-sm text-indigo-600 hover:underline">
+              <Link
+                to="/"
+                className="relative w-fit cursor-pointer text-black dark:text-white after:content-[''] after:absolute after:left-0 after:-bottom-0 after:h-[1px] after:w-full after:bg-blue-400 after:scale-x-0 after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+              >
                 Forgot Password?
               </Link>
             </div>
@@ -100,17 +98,12 @@ const Login = () => {
             <Button
               content={isPending ? "Logging in..." : "Login"}
               pattern="outline"
-              className="w-50! mt-4 bg-white text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3
-               shadow-[4px_4px_0_0_#000] transition-all duration-200 ease-out"
+              className="w-60! mt-8 bg-white text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 shadow-[4px_4px_0_0_#000] transition-all duration-200 ease-out dark:bg-black dark:text-white! dark:border-white dark:shadow-[4px_4px_0_0_#fff]"
               buttonProps={{ disabled: isPending, type: "submit" }}
             />
 
             {/* Divider */}
-            <div className="text-center mt-7">
-              <div className="flex items-center gap-3 mb-2">
-                <div className="flex-1 h-px bg-black/30" />
-              </div>
-            </div>
+            <div className="h-px dark:bg-white bg-black mt-7" />
 
             {/* Social Icons */}
             <div className="flex justify-center gap-5 mt-4 text-xl">
@@ -120,8 +113,7 @@ const Login = () => {
                   onClick: handleGoogleLogin,
                   type: "button",
                 }}
-                className=" w-60! bg-white text-gray-800 border border-gray-300 
-                rounded-lg text-sm shadow-[2px_2px_0_0_#000]"
+                className="w-60! mt-8 bg-white rounded-xl text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 shadow-[2px_2px_0_0_#000] transition-all duration-200 ease-out dark:bg-black dark:text-white! dark:border-white dark:shadow-[2px_2px_0_0_#fff]"
                 content={
                   <span className="flex items-center gap-2">
                     <img
@@ -140,36 +132,28 @@ const Login = () => {
             {/* Register */}
             <div className="mt-6 flex justify-center">
               <div className="flex flex-col justify-center items-center text-xs">
-                <span>Don’t have an account?</span>
+                <span className="text-black dark:text-gray-300">
+                  Don’t have an account?
+                </span>
 
-                <Button
-                  content="REGISTER"
-                  className="transition-all duration-300 hover:bg-gray-200 hover:rounded-full hover:shadow-2xl hover:shadow-blue-900 px-5 py-2"
-                  icons={{
-                    right: <FaArrowRight className="ml-1" />,
-                  }}
-                  buttonProps={{
-                    type:"button",
-                    onClick: () => navigate("/register"),
-                  }}
-                />
+                <Link
+                  to="/register"
+                  className="transition-all duration-300 hover:bg-gray-200 hover:rounded-full hover:shadow-2xl hover:shadow-blue-900 px-5 py-2 dark:text-white dark:hover:bg-gray-800 dark:hover:shadow-blue-400"
+                >
+                  REGISTER
+                </Link>
               </div>
             </div>
           </form>
         </div>
       </div>
       {/* Back to Home */}
-      <Button
-        content="🏠 Back to Home"
-        pattern="outline"
-        className="h-10! w-50! mt-4 bg-black text-white border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 transition-all
-                     duration-200 ease-out hover:bg-white hover:text-black hover:shadow-[4px_4px_0_0_#000]"
-        icons={{ right: <FaArrowRight /> }}
-        buttonProps={{
-          type:"button",
-          onClick: () => navigate("/")
-        }}
-      />
+      <Link
+        to="/"
+        className=" w-50! bg-black text-white border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 transition-all duration-200 ease-out hover:bg-white hover:text-black hover:shadow-[4px_4px_0_0_#000] dark:bg-white dark:text-black dark:border-white  dark:hover:bg-black dark:hover:text-white dark:hover:shadow-[4px_4px_0_0_#fff]"
+      >
+        🏠 Back to Home
+      </Link>
     </div>
   );
 };
