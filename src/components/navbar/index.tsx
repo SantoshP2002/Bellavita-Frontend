71778a70a4f9b52d71792bcd1efcc77217972084
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-13 15:41:42 +0530

    navbar component in fix dark mode css

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   return (
-    <div className="w-full shadow sticky bg-white top-0 z-50">
+    <div className="w-full shadow sticky bg-white dark:bg-black dark:text-white top-0 z-50">
       <div className="flex items-center">
         <MobileBar />
         <HeaderAction />
@@ -37,18 +37,18 @@ const Navbar = () => {
 
             {/* Dropdown Menu */}
             {selectedOption === item.name && item.options && (
-              <div className="absolute left-0 top-6 bg-white rounded-md p-7 flex flex-col gap-2 z-50 min-w-[200px]">
+              <div className="absolute left-0 top-6 bg-white dark:bg-black rounded-md p-7 flex flex-col gap-2 z-50 min-w-[200px]">
                 {item.options.map((option, index) => (
                   <div
                     key={index}
-                    className="relative w-fit whitespace-nowrap cursor-pointer group/option text-black hover:text-gray-600 text-xs"
+                    className="relative w-fit whitespace-nowrap cursor-pointer group/option hover:text-gray-600 text-xs"
                     onClick={() => {
                       navigate(
                         `/products?category=${item.value}&subCategory=${option.value}`
                       );
                     }}
                   >
-                    <span>{option.name}</span>
+                    <span className=" dark:text-white">{option.name}</span>
                     <span className="absolute left-0 bottom-0 h-[1.5px] w-0 bg-gray-300 transition-all duration-300 group-hover/option:w-full" />
                   </div>
                 ))}
