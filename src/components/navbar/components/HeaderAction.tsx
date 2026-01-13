423a586d2fb08a488abd1ffaea1d50f5fa910832
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-13 15:40:23 +0530

    navbar/HeaderAction component in fix dark mode css

--- src/components/navbar/components/HeaderAction.tsx
+++ src/components/navbar/components/HeaderAction.tsx
@@ -28,21 +28,22 @@ const HeaderAction = () => {
           isOpen={isSearchOpen}
           onClose={() => setIsSearchOpen(false)}
           heading="Search Products"
+          className="dark:bg-black dark:text-white"
         >
           <SearchModal onClose={() => setIsSearchOpen(false)} />
         </Modal>
       )}
-      <div className="flex-1 flex items-center justify-between">
+      <div className="flex-1 flex items-center justify-between dark:text-white px-4 md:px-8 py-3">
         <Logo />
         <div className="flex items-center gap-5 text-gray-700 px-8">
           <GoSearch
             onClick={() => setIsSearchOpen(true)}
-            className="h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 hover:text-indigo-600"
+            className="h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 dark:text-white"
           />
 
           {user?.role === "ADMIN" && (
             <Link to="/admin">
-              <GrUserAdmin className="h-4 w-4 md:h-7 md:w-7 [&>path]:stroke-[1.2]" />
+              <GrUserAdmin className="h-4 w-4 md:h-7 md:w-7 [&>path]:stroke-[1.2] dark:text-white" />
             </Link>
           )}
 
@@ -50,7 +51,7 @@ const HeaderAction = () => {
             <img
               src={user?.profilePic}
               alt="User"
-              className="border-2 border-black h-6 w-6 md:h-7 md:w-7 rounded-full cursor-pointer"
+              className="border-2 border-black dark:border-white h-6 w-6 md:h-7 md:w-7 rounded-full cursor-pointer"
               onClick={() => (isLoggedIn ? logout() : navigate("/login"))}
             />
           ) : (
@@ -62,12 +63,12 @@ const HeaderAction = () => {
 
           <div className="relative">
             <IoCartOutline
-              className="cursor-pointer transition-colors duration-200 hover:text-red-600 h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
+              className="cursor-pointer transition-colors duration-200 dark:text-white h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
               onClick={() => navigate("/cart")}
             />
 
             {cartCount > 0 && (
-              <span className="absolute -top-2 -right-2 bg-red-600 text-white font-bold rounded-2xl flex items-center justify-center h-4 w-4 text-[10px] sm:h-4.5 sm:w-4.5 sm:text-[11px] md:h-5 md:w-5 md:text-xs">
+              <span className="absolute -top-2 -right-2 bg-red-300 dark:bg-white text-black font-bold rounded-2xl flex items-center justify-center h-4 w-4 text-[10px] sm:h-4.5 sm:w-4.5 sm:text-[11px] md:h-5 md:w-5 md:text-xs">
                 {cartCount}
               </span>
             )}
@@ -75,11 +76,11 @@ const HeaderAction = () => {
 
           <CiDeliveryTruck
             onClick={() => navigate("/orders")}
-            className="h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 hover:text-indigo-600"
+            className="h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 dark:text-white"
           />
           <RiAccountBox2Line
             onClick={() => navigate("/profile")}
-            className="hidden md:block h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 hover:text-indigo-600"
+            className="hidden md:block h-4 w-4 md:h-7 md:w-7 cursor-pointer transition-colors duration-200 dark:text-white"
           />
         </div>
       </div>
