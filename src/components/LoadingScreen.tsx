67fb6f5ab081459b511e1423200d616ec06956d0
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-15 19:14:53 +0530

    LoadingScreen component in dark mode css fix

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,8 +1,8 @@
 const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-      <div className="relative w-32 h-[2px] overflow-hidden bg-gray-200 rounded-full">
-        <div className="absolute left-0 top-0  h-full w-1/1 bg-black animate-slide rounded-full" />
+    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-black z-50">
+      <div className="relative w-32 h-[2px] overflow-hidden bg-gray-200 dark:bg-gray-600 rounded-full">
+        <div className="absolute left-0 top-0  h-full w-1/1 bg-black dark:bg-white animate-slide rounded-full" />
       </div>
     </div>
   );
