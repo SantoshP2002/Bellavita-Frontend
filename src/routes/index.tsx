b974a106a282901af691fcb1db471a1332374659
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-18 16:20:09 +0530

    routing in search Page file name changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -42,7 +42,7 @@ import Profile from "../pages/profile/Profile";
 import EditProfile from "../pages/profile/EditProfile";
 import ChangePassword from "../pages/profile/ChangePassword";
 import CrazyDealsMobile from "../components/CrazyDealsMobile";
-import SearchPage from "../pages/search/index";
+import SearchPage from "../pages/search";
 
 const router = createBrowserRouter([
   {
