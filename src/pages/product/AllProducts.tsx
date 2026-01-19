83d40bcbf88cd0647e573958459ad6a7400323a1
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-19 18:22:49 +0530

    All Product page in css changes

--- src/pages/product/AllProducts.tsx
+++ src/pages/product/AllProducts.tsx
@@ -7,7 +7,7 @@ import EmptyData from "../../components/empty-data/EmptyData";
 import LoadingScreen from "../../components/LoadingScreen";
 import { Button } from "../../components/Button";
 import Select from "../../components/Select";
-import { SORT_DATA } from "../../constants";
+import { CATEGORY_IMAGES, SORT_DATA } from "../../constants";
 import { IoIosArrowDown } from "react-icons/io";
 import { useAddToCart } from "../../api/cart/service";
 import { useUserStore } from "../../store/user";
@@ -49,6 +49,11 @@ const AllProducts = () => {
   const products = data?.pages?.flatMap((page) => page.products) || [];
   const totalProducts = data?.pages?.[0]?.totalProducts || 0;
 
+  const currentCategory = queryParams.subCategory || queryParams.category;
+
+  const categoryImage =
+    currentCategory && CATEGORY_IMAGES[currentCategory.toLowerCase()];
+
   const handleSortChange = (value: string) => {
     if (value) setParams({ sortBy: value });
     else removeParam("sortBy");
@@ -61,116 +66,127 @@ const AllProducts = () => {
   if (isError) return <h1>{error.message}</h1>;
 
   return (
-    <div className="py-4 px-3 sm:px-6 md:px-12 lg:px-24 dark:bg-black dark:text-white">
-      <p className="text-xl mt-3 sm:text-2xl text-center capitalize mb-2">
-        -{" "}
-        {queryParams.subCategory
-          ? queryParams.subCategory.replace("_", " ")
-          : "All Products"}{" "}
-        -
-      </p>
-      {/* NEEDLE LINE */}
-      <span className="mx-auto block h-[2px] w-[40%] bg-gradient-to-r from-transparent via-gray-400 to-transparent dark:via-gray-500 mb-10" />
-
-      {/* Filter and Sort Bar */}
-      <div className="mt-4 flex flex-col sm:flex-row items-center justify-between gap-3 sm:gap-0">
-        <div className="flex flex-wrap gap-2 items-center w-full sm:w-auto justify-center sm:justify-start">
-          {/* FILTER BUTTON */}
-          <Button
-            content="FILTER +"
-            pattern="outline"
-            className="!w-fit !h-9 text-sm px-4 py-1.5 rounded"
-            buttonProps={{
-              onClick: handleFilter,
-            }}
-          />
-          {/* SORT SELECT OPTIONS */}
-          <Select
-            containerClassName="!w-40 sm:!w-50 !h-10 !m-1 cursor-pointer"
-            icons={{
-              right: {
-                icon: <IoIosArrowDown className="text-base cursor-pointer" />,
-              },
-            }}
-            selectProps={{
-              onChange: (data) => handleSortChange(data.value),
-              options: SORT_DATA,
-              value: { name: "SORT BY", value: queryParams.sortBy || "" },
-            }}
+    <div>
+      {categoryImage && (
+        <div className="mx-auto w-full h-[90px] md:h-[300px]">
+          <img
+            src={categoryImage}
+            alt={currentCategory}
+            className="w-full h-full object-cover"
           />
         </div>
-        {/* Total product count  */}
-        <p className="text-xs sm:text-sm text-gray-600 font-medium mt-1 sm:mt-0 dark:text-green-500">
-          {totalProducts} Product{totalProducts !== 1 ? "s" : ""}
+      )}
+      <div className="py-4 px-3 sm:px-6 md:px-12 lg:px-24 dark:bg-black dark:text-white">
+        <p className="text-xl mt-3 sm:text-2xl text-center capitalize mb-2">
+          -{" "}
+          {queryParams.subCategory
+            ? queryParams.subCategory.replace("_", " ")
+            : "All Products"}{" "}
+          -
         </p>
-      </div>
+        {/* NEEDLE LINE */}
+        <span className="mx-auto block h-[2px] w-[40%] bg-gradient-to-r from-transparent via-gray-400 to-transparent dark:via-gray-500 mb-10" />
 
-      {/* Product Grid */}
-      {isLoading ? (
-        <LoadingScreen content="All Product Loading Please Wait !" />
-      ) : isError ? (
-        <EmptyData content="Product Not Found " />
-      ) : products.length > 0 ? (
-        <div className="py-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 sm:gap-6">
-          {products.map((p, index) => {
-            const isLastItem = index === products.length - 4;
-            return (
-              <div
-                key={p._id}
-                ref={isLastItem ? ref : null}
-                className="w-full bg-white dark:bg-black rounded- overflow-hidden transition-shadow duration-300"
-              >
-                {/* Image */}
-                <div className="relative h-64 sm:h-60 md:h-72 w-full bg-gray-100">
-                  <img
-                    src={p.images?.[0]}
-                    alt={p.title}
-                    onClick={() => navigate(p?._id)}
-                    className="w-full h-full object-contain p-3 sm:p-4 hover:scale-105 transition-transform duration-300 cursor-pointer"
-                  />
-                </div>
+        {/* Filter and Sort Bar */}
+        <div className="mt-4 flex flex-col sm:flex-row items-center justify-between gap-3 sm:gap-0">
+          <div className="flex flex-wrap gap-2 items-center w-full sm:w-auto justify-center sm:justify-start">
+            {/* FILTER BUTTON */}
+            <Button
+              content="FILTER +"
+              pattern="outline"
+              className="!w-fit !h-9 text-sm px-4 py-1.5 rounded"
+              buttonProps={{
+                onClick: handleFilter,
+              }}
+            />
+            {/* SORT SELECT OPTIONS */}
+            <Select
+              containerClassName="!w-40 sm:!w-50 !h-10 !m-1 cursor-pointer"
+              icons={{
+                right: {
+                  icon: <IoIosArrowDown className="text-base cursor-pointer" />,
+                },
+              }}
+              selectProps={{
+                onChange: (data) => handleSortChange(data.value),
+                options: SORT_DATA,
+                value: { name: "SORT BY", value: queryParams.sortBy || "" },
+              }}
+            />
+          </div>
+          {/* Total product count  */}
+          <p className="text-xs sm:text-sm text-gray-600 font-medium mt-1 sm:mt-0 dark:text-green-500">
+            {totalProducts} Product{totalProducts !== 1 ? "s" : ""}
+          </p>
+        </div>
 
-                {/* Details */}
-                <div className="p-2 sm:p-3 dark:bg-black dark:text-white">
-                  <p className="text-[10px] sm:text-xs text-gray-300 font-medium line-clamp-1">
-                    {p.brand}
-                  </p>
-                  <h2 className="text-xs sm:text-sm mt-1 font-semibold line-clamp-1">
-                    {p.title}
-                  </h2>
-                  <div className="mt-2 flex items-center gap-2">
-                    <p className="text-sm sm:text-base font-bold">
-                      ₹{p.sellingPrice.toFixed(2)}
+        {/* Product Grid */}
+        {isLoading ? (
+          <LoadingScreen content="All Product Loading Please Wait !" />
+        ) : isError ? (
+          <EmptyData content="Product Not Found " />
+        ) : products.length > 0 ? (
+          <div className="py-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 sm:gap-6">
+            {products.map((p, index) => {
+              const isLastItem = index === products.length - 4;
+              return (
+                <div
+                  key={p._id}
+                  ref={isLastItem ? ref : null}
+                  className="w-full bg-white dark:bg-black rounded- overflow-hidden transition-shadow duration-300"
+                >
+                  {/* Image */}
+                  <div className="relative h-64 sm:h-60 md:h-72 w-full bg-gray-100">
+                    <img
+                      src={p.images?.[0]}
+                      alt={p.title}
+                      onClick={() => navigate(p?._id)}
+                      className="w-full h-full object-contain p-3 sm:p-4 hover:scale-105 transition-transform duration-300 cursor-pointer"
+                    />
+                  </div>
+
+                  {/* Details */}
+                  <div className="p-2 sm:p-3 dark:bg-black dark:text-white">
+                    <p className="text-[10px] sm:text-xs text-gray-300 font-medium line-clamp-1">
+                      {p.brand}
                     </p>
-                    {p.price > p.sellingPrice && (
-                      <p className="text-xs sm:text-sm text-gray-400 line-through">
-                        ₹{p.price.toFixed(2)}
+                    <h2 className="text-xs sm:text-sm mt-1 font-semibold line-clamp-1">
+                      {p.title}
+                    </h2>
+                    <div className="mt-2 flex items-center gap-2">
+                      <p className="text-sm sm:text-base font-bold">
+                        ₹{p.sellingPrice.toFixed(2)}
                       </p>
-                    )}
+                      {p.price > p.sellingPrice && (
+                        <p className="text-xs sm:text-sm text-gray-400 line-through">
+                          ₹{p.price.toFixed(2)}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+
+                  {/* Add to Cart */}
+                  <div className="py-1 px-3">
+                    <Button
+                      content="Add To Cart"
+                      pattern="outline"
+                      className="w-30! lg:w-60!"
+                      buttonProps={{
+                        onClick: () => handleAddToCart(p._id),
+                      }}
+                    />
                   </div>
                 </div>
-
-                {/* Add to Cart */}
-                <div className="py-1 px-3">
-                  <Button
-                    content="Add To Cart"
-                    pattern="outline"
-                    className="w-30! lg:w-60!"
-                    buttonProps={{
-                      onClick: () => handleAddToCart(p._id),
-                    }}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-        <EmptyData
-          content={"No Product Found"}
-          className="w-full h-[50dvh] mx-auto [&>h3]:text-base sm:[&>h3]:text-2xl [&>h3]:uppercase gap-5"
-        />
-      )}
+              );
+            })}
+          </div>
+        ) : (
+          <EmptyData
+            content={"No Product Found"}
+            className="w-full h-[50dvh] mx-auto [&>h3]:text-base sm:[&>h3]:text-2xl [&>h3]:uppercase gap-5"
+          />
+        )}
+      </div>
     </div>
   );
 };
