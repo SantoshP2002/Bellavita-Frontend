f9a4f784235c0c80735403e195a11ecef91fb408
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-26 17:58:42 +0530

    navbar/index.tsx  in dropdown css changes

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -37,19 +37,21 @@ const Navbar = () => {
 
             {/* Dropdown Menu */}
             {selectedOption === item.name && item.options && (
-              <div className="absolute left-0 top-6 bg-white dark:bg-black rounded-md p-7 flex flex-col gap-2 z-50 min-w-[200px]">
+              <div className="absolute left-0 top-full z-50 min-w-[200px] rounded-xl bg-white p-6 flex flex-col shadow-[0_10px_30px_rgba(0,0,0,0.12)] animate-dropdown origin-top dark:bg-black dark:text-whit">
                 {item.options.map((option, index) => (
                   <div
                     key={index}
-                    className="relative w-fit whitespace-nowrap cursor-pointer group/option hover:text-gray-600 text-xs"
-                    onClick={() => {
+                    onClick={() =>
                       navigate(
                         `/products?category=${item.value}&subCategory=${option.value}`,
-                      );
-                    }}
+                      )
+                    }
+                    className="relative cursor-pointer whitespace-nowrap text-sm text-gray-700 px-2 py-1 rounded-md transition-all duration-200 hover:text-black dark:hover:text-white dark:text-white hover:translate-x-1 group/option"
                   >
-                    <span className=" dark:text-white">{option.name}</span>
-                    <span className="absolute left-0 bottom-0 h-[1.5px] w-0 bg-gray-300 transition-all duration-300 group-hover/option:w-full" />
+                    {option.name}
+
+                    {/* underline animation */}
+                    <span className="absolute left-2 bottom-0 h-[1.5px] w-0 bg-gray-400 transition-all duration-300 group-hover/option:w-[70%]" />
                   </div>
                 ))}
               </div>
