fa15dca2a654efeba7caaa7db5b9473fcaed1a30
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-26 20:39:56 +0530

    AddressPage in checkout button css changes

--- src/pages/address/Address.tsx
+++ src/pages/address/Address.tsx
@@ -85,7 +85,7 @@ const Address = () => {
           <Button
             content="Checkout"
             pattern="outline"
-            className="mt-10 bg-black text-white border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 transition-all duration-200 ease-out hover:bg-white hover:text-black hover:shadow-[4px_4px_0_0_#000] dark:bg-white dark:text-black dark:border-white dark:hover:bg-black dark:hover:text-white dark:hover:shadow-[4px_4px_0_0_#fff]"
+            className="mt-8!"
             buttonProps={{
               onClick: () => {
                 if (!selectedAddress) {
@@ -105,7 +105,7 @@ const Address = () => {
             <Button
               content="BACK"
               pattern="outline"
-              className="w-40! mt-8 bg-white text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 shadow-[4px_4px_0_0_#000] transition-all duration-200 ease-out dark:bg-black dark:text-white! dark:border-white dark:shadow-[4px_4px_0_0_#fff]"
+              className="mt-8! w-90! bg-white text-black border-2 border-black text-xs sm:text-sm py-1 sm:py-2 px-3 shadow-[4px_4px_0_0_#000] transition-all duration-200 ease-out dark:bg-black dark:text-white! dark:border-white dark:shadow-[4px_4px_0_0_#fff]"
               buttonProps={{ type: "button" }}
             />
           </Link>
