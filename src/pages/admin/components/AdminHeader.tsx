4198272b6a67cd3b783bc9763e73efc106ef7b6d
SantoshP2002 <santoshpawar96k@gmail.com>
2026-01-28 14:35:58 +0530

    admin/page  AdminHeaders component in css changes and user functionality added

--- src/pages/admin/components/AdminHeader.tsx
+++ src/pages/admin/components/AdminHeader.tsx
@@ -1,11 +1,15 @@
 import { FaBars } from "react-icons/fa";
 import { Button } from "../../../components/Button";
+import { useUserStore } from "../../../store/user";
+import { CiUser } from "react-icons/ci";
 
 type AdminHeadersProps = {
   onMenuClick?: () => void;
 };
 
 const AdminHeader = ({ onMenuClick }: AdminHeadersProps) => {
+  const { user } = useUserStore();
+
   return (
     <div className="dark:bg-black dark:text-white">
       <header className="flex justify-between items-center p-4 border-b-2 rounded-lg">
@@ -24,14 +28,28 @@ const AdminHeader = ({ onMenuClick }: AdminHeadersProps) => {
 
         {/* Right Section */}
         <div className="flex items-center gap-3">
-          <span className="text-gray-600 hidden sm:block">
-            Welcome, Admin 👋
-          </span>
-          <img
-            src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-118143566.jpg"
-            alt="Admin"
-            className="w-10 h-10 rounded-full"
-          />
+          {/* Profile Image / Icon */}
+          {user?.profilePic ? (
+            <img
+              src={user.profilePic}
+              alt="profile"
+              className="w-10 h-10 rounded-full object-cover"
+            />
+          ) : (
+            <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
+              <CiUser className="w-6 h-6 text-gray-600" />
+            </div>
+          )}
+
+          {/* Text Section */}
+          <div className="flex flex-col leading-tight">
+            <span className="dark:text-gray-200 hidden sm:block">
+              Welcome, Admin 👋
+            </span>
+            <p className="font-semibold text-xl underline uppercase dark:text-white transform transform-3d">
+              {user?.firstName}
+            </p>
+          </div>
         </div>
       </header>
     </div>
