ab1f768cf47c7285cea321031863a87ac5e6e523
SantoshP2002 <santoshpawar96k@gmail.com>
2026-04-06 18:09:10 +0530

    Create CartProductModal component

---
+++ src/components/modal/children/CartProductModal.tsx
@@ -0,0 +1,39 @@
+import Modal from "../index";
+import CartProducts from "../../../components/CartProducts";
+import { motion, AnimatePresence } from "framer-motion";
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+
+const CartProductModal = ({ open, onClose }: Props) => {
+  return (
+    <Modal
+      isOpen={open}
+      onClose={onClose}
+      heading="My Cart"
+      className="max-w-6xl h-[90vh] overflow-hidden rounded-2xl bg-white dark:bg-black"
+    >
+      <AnimatePresence mode="wait">
+        {open && (
+          <motion.div
+            key="cartModal"
+            initial={{ opacity: 0, scale: 0.95, y: 30 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.95, y: 30 }}
+            transition={{ duration: 0.25 }}
+            className="flex h-full min-h-0 flex-col"
+          >
+            {/* Scroll sirf content area me hoga */}
+            <div className="min-h-0 flex-1 overflow-y-auto bg-white dark:bg-black px-3 sm:px-4 md:px-6 pb-4 sm:pb-6">
+              <CartProducts onClose={onClose} />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Modal>
+  );
+};
+
+export default CartProductModal;
